94a49fe3c978e0231c17fcc2d9ee733a37af7fec
Gitea <gitea@fake.local>
2022-03-17 15:57:46 +0800

    Commit from Win7

---
+++ src/main/java/com/mike/jdbcdemo/utils/JsonUtils.java
@@ -0,0 +1,45 @@
+package com.mike.jdbcdemo.utils;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JsonUtils {
+
+    private final static Logger logger = LoggerFactory.getLogger(JsonUtils.class);
+
+    public static <POJO> POJO getObject(String json, final Class<POJO> clazz) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        try {
+            mapper.setVisibility(mapper.getSerializationConfig().getDefaultVisibilityChecker().withFieldVisibility(JsonAutoDetect.Visibility.ANY).withGetterVisibility(JsonAutoDetect.Visibility.NONE).withSetterVisibility(
+                    JsonAutoDetect.Visibility.NONE).withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            return mapper.readValue(json, clazz);
+        }
+        catch (Exception e) {
+            logger.error("JsonReader Error", e);
+            return null;
+        }
+    }
+    public static <POJO> String getJson(POJO object) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.setVisibility(mapper.getSerializationConfig().getDefaultVisibilityChecker().withFieldVisibility(JsonAutoDetect.Visibility.ANY).withGetterVisibility(JsonAutoDetect.Visibility.NONE).withSetterVisibility(
+                    JsonAutoDetect.Visibility.NONE).withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+            return mapper.writeValueAsString(object);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            logger.error("JsonWriter Error", e);
+            return null;
+        }
+    }
+}
