f10a4b999b0d30e8fd3dc405de9b5000207c2d47
Gitea <gitea@fake.local>
2022-03-17 16:05:07 +0800

    Commit by win7

--- src/main/java/com/mike/jdbcdemo/utils/JsonUtils.java
+++ src/main/java/com/mike/jdbcdemo/utils/JsonUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
